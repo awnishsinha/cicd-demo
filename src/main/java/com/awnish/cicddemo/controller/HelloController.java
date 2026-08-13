0074316dafbb6f6b5ef103801e7684c438a92b23
awnish sinha <awnish@dataprabanjam.sg>
2026-08-13 09:28:05 -0700

    Green Server For Testing

--- src/main/java/com/awnish/cicddemo/controller/HelloController.java
+++ src/main/java/com/awnish/cicddemo/controller/HelloController.java
@@ -8,11 +8,11 @@ public class HelloController {
 
     @GetMapping("/api/hello")
     public String hello(){
-        return "Hello Version-2 from CI/CD";
+        return "Hello Version-Green from CI/CD";
     }
 
     @GetMapping("/api/welcome")
     public String welcome(){
-        return "Welcome from CI/CD";
+        return "Welcome to Version-Green from CI/CD";
     }
 }
